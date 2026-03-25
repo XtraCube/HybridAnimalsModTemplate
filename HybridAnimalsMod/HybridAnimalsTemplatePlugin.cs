b3fc6d1793cd9b4bd97fc14f4b7f7c255487f208
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-03-25 18:54:06 -0400

    init

---
+++ HybridAnimalsMod/HybridAnimalsTemplatePlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace HybridAnimalsMod;
+
+[BepInAutoPlugin]
+public partial class HybridAnimalsTemplatePlugin : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+
+
+    public override void Load()
+    {
+        Harmony.PatchAll();
+    }
+}
