c1e7692a76491769b4eb680505112db0e8193eb7
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-03-25 19:39:16 -0400

    fixed build

--- HybridAnimalsMod/HybridAnimalsTemplatePlugin.cs
+++ HybridAnimalsMod/HybridAnimalsTemplatePlugin.cs
@@ -9,9 +9,17 @@ public partial class HybridAnimalsTemplatePlugin : BasePlugin
 {
     public Harmony Harmony { get; } = new(Id);
 
-
     public override void Load()
     {
         Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(AdvertControl), nameof(AdvertControl.LaunchAds))]
+    public static class TestPatch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
     }
 }
